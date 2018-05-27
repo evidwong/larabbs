a22214f0daa4fc7cba7ae6251249eb0e17366274
Evid Wong <76572677@qq.com>
2018-05-27 16:53:08 +0000

    Basic user profile editing

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 
 class UsersController extends Controller
@@ -10,5 +11,12 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show',compact('user'));
+    }
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
+    public function update(User $user,UserRequest $request){
+        $user->update($request->all());
+        return redirect()->route('users.show',compact('user'))->with('success','修改成功');
     }
 }
