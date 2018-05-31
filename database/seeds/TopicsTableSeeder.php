fc9b60ba90f0d14715190bff1b03584de2d62558
Evid Wong <76572677@qq.com>
2018-05-31 16:48:59 +0000

    Seeding topics and users data

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -9,9 +9,13 @@ class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
+        // 所有用户 ID 数组，如：[1,2,3,4]
         $user_ids = User::all()->pluck('id')->toArray();
+        
+        // 所有分类 ID 数组，如：[1,2,3,4]
         $category_ids = Category::all()->pluck('id')->toArray();
 
+        // 获取 Faker 实例
         $faker = app(Faker\Generator::class);
 
         $topics = factory(Topic::class)
@@ -23,7 +27,7 @@ class TopicsTableSeeder extends Seeder
                 // 话题分类，同上
                 $topic->category_id = $faker->randomElement($category_ids);
             });
-        
+
         // 将数据集合转换为数组，并插入到数据库中
         Topic::insert($topics->toArray());
     }
